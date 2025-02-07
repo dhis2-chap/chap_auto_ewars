911ea06f8dc0e97ed5ca10a7c457f4257cc297a6
Halvardgithub <halvar3005@outlook.com>
2025-02-07 10:20:10 +0100

    small changes to inla formula

--- predict.R
+++ predict.R
@@ -7,11 +7,13 @@
 # rainsum = rainfall
 # meantemperature = mean_temperature
 
+options(warn=1)
+
 library(INLA)
 library(dlnm)
 library(dplyr)
 
-#for graphs
+#for spatial effects
 library(sf)
 library(spdep)
 
@@ -41,20 +43,12 @@ predict_chap <- function(model_fn, hist_fn, future_fn, preds_fn, graph_fn){
          arglag = list(fun = "ns", knots = 3/2), group = df$ID_spat)
   colnames(basis_rainsum) = paste0("basis_rainsum.", colnames(basis_rainsum))
   
-  
-  #also need some conversion from geojson file to adjacency matrix in R, obs for harmonization
-  #f(ID_spat, model = "icar", graph = adjacency_matrix), the ICAR formula, can also use a BYM
-  # just ICAR + iid for the spatial regions
-  #lagged_formula <- Cases ~ 1 + f(ID_spat, model='iid', replicate=ID_year) + 
-  #  f(month, model='rw1', cyclic=T, scale.model=T) + basis_meantemperature + basis_rainsum
-  
   df$ID_spat <- as.factor(df$ID_spat)
   df$ID_spat_num <- as.numeric(as.factor(df$ID_spat))
   
   df <- cbind(df, basis_meantemperature, basis_rainsum)
   
-  #formula without a yearly effect, instead a common rw1 for all regions for the months, still
-  #has a iid region specific effect and the cyclic rw1 over the months, plus the exogenous vars
+  # the formula without spatial smoothing and with some specified PC priors
   #lagged_formula <- Cases ~ 1 + f(ID_spat, model='iid', hyper=list(prec = list(prior = "pc.prec",
   #  param = c(1, 0.01)))) + f(month_num, model = "rw1", scale.model = T,
   #  replicate = ID_spat_num, hyper=list(prec = list(prior = "pc.prec", param = c(1, 0.01)))) +
@@ -66,7 +60,7 @@ predict_chap <- function(model_fn, hist_fn, future_fn, preds_fn, graph_fn){
     f(month, model='rw1', cyclic=T, scale.model=T) + basis_meantemperature + basis_rainsum
  
   if(graph_fn != ""){
-    #df$ID_spat_num2 <- df$ID_spat_num
+    df$ID_spat_num2 <- df$ID_spat_num
     
     geojson <- st_read(graph_fn)
     geojson <- st_make_valid(geojson) #some geojson files will giver errors later if this is not cleaned here
@@ -80,15 +74,12 @@ predict_chap <- function(model_fn, hist_fn, future_fn, preds_fn, graph_fn){
     nb <- poly2nb(geojson_red, queen = TRUE) #adjacent polygons are neighbors
     adjacency <- nb2mat(nb, style = "B", zero.policy = TRUE) #converts it to an adjacency matrix which is passed to the bym2
     
-    # lagged_formula <- Cases ~ 1 + f(ID_spat_num, model = "bym2", graph = adjacency) + 
-    #   f(month_num, model = "rw1", scale.model = T,
-    #   replicate = ID_spat_num, hyper=list(prec = list(prior = "pc.prec", param = c(1, 0.01)))) +
-    #   f(month, model='rw1', cyclic=T, scale.model=T, hyper=list(prec = list(prior = "pc.prec",
-    #   param = c(1, 0.01)))) + basis_meantemperature + basis_rainsum
+    #lagged_formula <- Cases ~ 1 + f(ID_spat_num, model = "bym2", graph = adjacency) +
+    #  f(month, model='rw1', cyclic=T, scale.model=T) + basis_meantemperature + basis_rainsum
     
-    lagged_formula <- Cases ~ 1 + f(ID_spat_num, model = "bym2", graph = adjacency, replicate = ID_year) + 
-      f(month_num, model = "rw1", scale.model = T, replicate = ID_spat_num) +
-      f(month, model='rw2', cyclic=T, scale.model=T) + basis_meantemperature + basis_rainsum
+    lagged_formula <- Cases ~ 1 + f(ID_spat_num, model = "besag", graph = adjacency, scale.model = T) +
+      f(ID_spat_num2, model = "iid") + f(month_num, model = "rw1", scale.model = T) +
+       f(month, model='rw1', cyclic=T, scale.model=T) + basis_meantemperature + basis_rainsum
   }
   
   model <- inla(formula = lagged_formula, data = df, family = "nbinomial", offset = log(E),
@@ -128,7 +119,6 @@ predict_chap <- function(model_fn, hist_fn, future_fn, preds_fn, graph_fn){
 args <- commandArgs(trailingOnly = TRUE)
 
 if (length(args) >= 1) {
-  cat("running predictions")
   model_fn <- args[1]
   hist_fn <- args[2]
   future_fn <- args[3]
@@ -138,15 +128,6 @@ if (length(args) >= 1) {
   predict_chap(model_fn, hist_fn, future_fn, preds_fn, graph_fn)
 }
 
-# #plot of Vietnam for all the regions
-# plot(st_geometry(geojson),
-#      col = "steelblue", lwd = 0.5, asp = 1)
-# 
-# #plot of the regions of Vietnam in the training data
-# plot(st_geometry(geojson_red),
-#      col = "steelblue", lwd = 0.5, asp = 1)
-# 
-# 
 
 # testing
 #library(dplyr)
@@ -172,70 +153,39 @@ if (length(args) >= 1) {
 # df <- rbind(historic_df, df)
 # df <- offset_years_and_months(df)
 # df$ID_year <- df$ID_year - min(df$ID_year) + 1
-
-un <- unique(df$location)
-df_loc1 <- df[df$location == un[3], ][c("rainfall", "disease_cases")]
-
-plot(model$summary.random$month_num$mean[800:1000])
-
-library(ggplot2)
-
-df_loc1$t <- 1:nrow(df_loc1)  # Create a time index from 1 to n
-df_long <- df_loc1 %>% pivot_longer(cols = -t, names_to = "variable", values_to = "value")
-
-ggplot(df_long, aes(x = t, y = value, color = variable)) +
-  geom_line() +
-  labs(title = "title", 
-       x = "x", y = "y") +
-  theme(legend.title = element_blank())
-
-library(zoo)
-df_loc1$rainfall <- na.locf(df_loc1$rainfall)
-df_loc1$disease_cases <- na.locf(df_loc1$disease_cases)
-
-ccf(df_loc1$rainfall, df_loc1$disease_cases, 
-    main = "Cross-Correlation between Rainfall and Disease Cases")
-acf(df_loc1$disease_cases)
-
-
-
-# basis_meantemperature <- extra_fields(df)
-# basis_rainsum <- get_basis_rainsum(df)
+#
+# # some data exploration with acf and ccf from timeseries analysis
+#
+# un <- unique(df$location)
+# df_loc1 <- df[df$location == un[3], ][c("rainfall", "disease_cases")]
 # 
-# basis_dlnm_meantemperature <- crossbasis(df$mean_temperature, lag=3, 
-#               argvar = list(fun = "ns", knots = equalknots(df$mean_temperature, 2)), 
-#               arglag = list(fun = "ns", knots = 3/2), group = df$ID_spat)
+# plot(model$summary.random$month_num$mean[800:1000])
 # 
-# check_equal <- basis_meantemperature == basis_dlnm_meantemperature
-# all(check_equal[!is.na(check_equal)]) #check if it worked for meantemp
+# library(ggplot2)
 # 
-# basis_dlnm_rainsum <- crossbasis(df$rainsum, lag=3, 
-#                                          argvar = list(fun = "ns", knots = equalknots(df$rainsum, 2)), 
-#                                          arglag = list(fun = "ns", knots = 3/2), group = df$ID_spat)
+# df_loc1$t <- 1:nrow(df_loc1)  # Create a time index from 1 to n
+# df_long <- df_loc1 %>% pivot_longer(cols = -t, names_to = "variable", values_to = "value")
 # 
-# check_equal2 <- basis_rainsum == basis_dlnm_rainsum
-# all(check_equal2[!is.na(check_equal2)])
+# ggplot(df_long, aes(x = t, y = value, color = variable)) +
+#   geom_line() +
+#   labs(title = "title", 
+#        x = "x", y = "y") +
+#   theme(legend.title = element_blank())
+# 
+# library(zoo)
+# df_loc1$rainfall <- na.locf(df_loc1$rainfall)
+# df_loc1$disease_cases <- na.locf(df_loc1$disease_cases)
+# 
+# ccf(df_loc1$rainfall, df_loc1$disease_cases, 
+#     main = "Cross-Correlation between Rainfall and Disease Cases")
+# acf(df_loc1$disease_cases)
 
 # 
-# lagged_formula <- Cases ~ 1 + f(ID_spat, model='iid', replicate=ID_year) + 
-#   f(month, model='rw1', cyclic=T, scale.model=T, replicate=ID_spat) + basis_meantemperature + basis_rainsum
-# 
-# model2 <- inla(formula = lagged_formula, data = df, family = "nbinomial", offset = log(E),
-#               control.inla = list(strategy = 'adaptive'),
-#               control.compute = list(dic = TRUE, config = TRUE, cpo = TRUE, return.marginals = FALSE),
-#               control.fixed = list(correlation.matrix = TRUE, prec.intercept = 1, prec = 1),
-#               control.predictor = list(link = 1, compute = TRUE),
-#               verbose = T, safe=FALSE)
-# 
-# rand_ID_spat2 <- model2$summary.random$ID_spat
-# 
-
-
-
+# # testing for geospatial components
 # geojson <- st_read(graph_fn)
 # geojson <- st_make_valid(geojson)
-
-# #might also need to alphabetically sort both geojson after location and the 
+# 
+# #might also need to alphabetically sort both geojson after location and the
 # # dataframe after location!
 # df <- read.csv(future_fn)
 # df_locs <- unique(df$location)
@@ -249,3 +199,101 @@ acf(df_loc1$disease_cases)
 # adjacency_red <- nb2mat(nb_red, style = "B", zero.policy = TRUE)
 # row.names(adjacency_red) <- NULL
 # image(adjacency_red)
+# 
+# #plot of Vietnam for all the regions
+# plot(st_geometry(geojson),
+#      col = "steelblue", lwd = 0.5, asp = 1)
+# 
+# #plot of the regions of Vietnam in the training data
+# plot(st_geometry(geojson_reduced),
+#      col = "steelblue", lwd = 0.5, asp = 1)
+# 
+# 
+# #For Brazil
+# 
+# graph_fn <- "brazil_polygons.geojson"
+# future_fn <- "brazil.csv"
+# df <- read.csv(future_fn)
+# df_locs <- unique(df$location)
+# geojson <- st_read(graph_fn)
+# geojson <- st_make_valid(geojson)
+# geojson$id
+# 
+# nb <- poly2nb(geojson, queen = TRUE) #adjacent polygons are neighbors
+# adjacency <- nb2mat(nb, style = "B", zero.policy = TRUE)
+# 
+# geojson_reduced <- geojson[geojson$id %in% df_locs, ]
+# nb_red <- poly2nb(geojson_reduced, queen = TRUE) #adjacent polygons are neighbors
+# adjacency_red <- nb2mat(nb_red, style = "B", zero.policy = TRUE)
+# row.names(adjacency_red) <- NULL
+# image(adjacency_red)
+# 
+# #plot of Vietnam for all the regions
+# plot(st_geometry(geojson),
+#      col = "steelblue", lwd = 0.5, asp = 1)
+# 
+# #plot of the regions of Vietnam in the training data
+# plot(st_geometry(geojson_reduced),
+#      col = "steelblue", lwd = 0.5, asp = 1)
+# 
+# df = read.csv(future_fn)
+# #df <- mutate(df, ID_year = ID_year - min(df$ID_year) + 1)
+# 
+# #adding a counting variable for the months like 1, ..., 12, 13, ...
+# #could also do years*12 + months, but fails for weeks
+# df <-group_by(df, location) |>
+#   mutate(month_num = row_number())
+# 
+# basis_meantemperature <- crossbasis(df$mean_temperature, lag=3,
+#                                     argvar = list(fun = "ns", knots = equalknots(df$mean_temperature, 2)),
+#                                     arglag = list(fun = "ns", knots = 3/2), group = df$ID_spat)
+# colnames(basis_meantemperature) = paste0("basis_meantemperature.", colnames(basis_meantemperature))
+# 
+# basis_rainsum <- crossbasis(df$rainsum, lag=3,
+#                             argvar = list(fun = "ns", knots = equalknots(df$rainsum, 2)),
+#                             arglag = list(fun = "ns", knots = 3/2), group = df$ID_spat)
+# colnames(basis_rainsum) = paste0("basis_rainsum.", colnames(basis_rainsum))
+# 
+# df$ID_spat <- as.factor(df$ID_spat)
+# df$ID_spat_num <- as.numeric(as.factor(df$ID_spat))
+# 
+# df <- cbind(df, basis_meantemperature, basis_rainsum)
+# 
+# # the formula without spatial smoothing and with some specified PC priors
+# #lagged_formula <- Cases ~ 1 + f(ID_spat, model='iid', hyper=list(prec = list(prior = "pc.prec",
+# #  param = c(1, 0.01)))) + f(month_num, model = "rw1", scale.model = T,
+# #  replicate = ID_spat_num, hyper=list(prec = list(prior = "pc.prec", param = c(1, 0.01)))) +
+# #  f(month, model='rw1', cyclic=T, scale.model=T, hyper=list(prec = list(prior = "pc.prec",
+# #  param = c(1, 0.01)))) + basis_meantemperature + basis_rainsum
+# 
+# lagged_formula <- Cases ~ 1 + f(ID_spat, model='iid') + f(month_num, model = "rw1", scale.model = T,
+#                                                           replicate = ID_spat_num) +
+#   f(month, model='rw1', cyclic=T, scale.model=T) + basis_meantemperature + basis_rainsum
+# 
+# if(graph_fn != ""){
+#   #df$ID_spat_num2 <- df$ID_spat_num
+#   
+#   geojson <- st_read(graph_fn)
+#   geojson <- st_make_valid(geojson) #some geojson files will giver errors later if this is not cleaned here
+#   
+#   df_locs <- unique(df$location)
+#   geojson_red <- geojson[geojson$VARNAME_1 %in% df_locs, ] #only keeps the regions present in the dataframe
+#   #both df_locs and geosjon_red should be in alfabetical order, believe the files are alfabetical in CHAP, might need to sort them
+#   #if they are both alphabetical, and take care about which field in the geeojson, could be vietnamese or english (handle in CHAP?)
+#   #then the regions should harmonize correctly in the formulas below
+#   
+#   nb <- poly2nb(geojson_red, queen = TRUE) #adjacent polygons are neighbors
+#   adjacency <- nb2mat(nb, style = "B", zero.policy = TRUE) #converts it to an adjacency matrix which is passed to the bym2
+#   
+#   lagged_formula <- Cases ~ 1 + f(ID_spat_num, model = "bym2", graph = adjacency, replicate = month_num) + 
+#     f(month_num, model = "rw1", scale.model = T, replicate = ID_spat_num) +
+#     f(month, model='rw2', cyclic=T, scale.model=T) + basis_meantemperature + basis_rainsum
+# }
+# 
+# model <- inla(formula = lagged_formula, data = df, family = "nbinomial", offset = log(E),
+#               control.inla = list(strategy = 'adaptive'),
+#               control.compute = list(config = TRUE, return.marginals = FALSE),
+#               control.fixed = list(correlation.matrix = TRUE, prec.intercept = 0.1, prec = 1),
+#               control.predictor = list(link = 1, compute = TRUE),
+#               verbose = F, safe=FALSE)
+# 
